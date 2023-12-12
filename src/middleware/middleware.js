c8ca309a1d4708c4bbd551323d4b196855c3d062
Akashi960 <Akashi960>
2023-12-12 00:12:07 -0500

    validacion eliminada

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -4,7 +4,7 @@ const validateNoNumbersOrSpecialChars = (value) => {
 };
 
 const validateCursoFields = (req, res, next) => {
-    const { nombre, descripcion, categoria, secciones } = req.body;
+    const { nombre, descripcion, categoria } = req.body;
 
     if (!validateNoNumbersOrSpecialChars(nombre)) {
         return res.status(400).json({
@@ -26,32 +26,6 @@ const validateCursoFields = (req, res, next) => {
             msg: 'El campo "descripcion" debe contener solo letras y espacios.',
         });
     }
-
-
-    //Validación para secciones y clases
-    
-    if (secciones && secciones.length > 0) {
-        for (const seccion of secciones) {
-            if (!validateNoNumbersOrSpecialChars(seccion.titulo)) {
-                return res.status(400).json({
-                    ok: false,
-                    msg: 'El campo "titulo" de una sección debe contener solo letras y espacios.',
-                });
-            }
-
-            if (seccion.clases && seccion.clases.length > 0) {
-                for (const clase of seccion.clases) {
-                    if (!validateNoNumbersOrSpecialChars(clase.titulo)) {
-                        return res.status(400).json({
-                            ok: false,
-                            msg: 'El campo "titulo" de una clase debe contener solo letras y espacios.',
-                        });
-                    }
-                }
-            }
-        }
-    }
-
     next(); // Si pasa la validación, pasa al siguiente middleware o controlador
 };
 
